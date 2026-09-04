cb2a0c1bf5c2bc5add332cb694b1cbe778ea34db
shifinozo <shifinshifu246@gmail.com>
2026-09-04 17:25:31 +0530

    feat: initialize root layout with custom fonts, metadata, tracking scripts, and floating contact components

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -66,6 +66,32 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-Y29NJJ1WBV');
           `}
         </Script>
+        {/* Facebook Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2870368053320401');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2870368053320401&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Facebook Pixel Code */}
       </body>
     </html>
   );
