e180977cff87a8557a1cc31ee872e01dabf8b22f
shifinozo <shifinshifu246@gmail.com>
2026-09-05 17:55:54 +0530

    feat: implement root layout with custom font configuration and global floating contact buttons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -50,48 +50,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${outfit.variable} ${instrumentSans.variable} ${roundo.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        {children}
-        <FloatingContactButtons />
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-Y29NJJ1WBV"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-Y29NJJ1WBV');
-          `}
-        </Script>
-        {/* Facebook Pixel Code */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window,document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2870368053320401');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2870368053320401&ev=PageView&noscript=1"
-            alt=""
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P93V5VC5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        {/* End Facebook Pixel Code */}
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+        <FloatingContactButtons />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P93V5VC5');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </body>
     </html>
   );
